9cbb3e038a0096ecda2fcab9e763b556aa0e7f43
Ana leticia <anaxzxl@gmail.com>
2024-02-28 18:56:33 -0300

    calculando o IMC

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -8,11 +8,11 @@ import com.google.android.material.textfield.TextInputEditText
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+         setContentView(R.layout.activity_main)
 
-        // Recuperar os componentes EditText
-             // Criar uma variável e associar o componente de UI <TextInputEditText>
-        // Recuperar o botão da tela
+        //Recuperar os componentes EditText
+            //Criar uma variavel e associar o componentes de UI<EditText>
+        //Recuperar o botão da tela
 
         val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
         val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
@@ -20,9 +20,14 @@ class MainActivity : AppCompatActivity() {
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
         btnCalcular.setOnClickListener {
-            val peso = edtPeso.text
-            println(peso)
+            val peso: Float = edtPeso.text.toString().toFloat()
+            val altura: Float = edtAltura.text.toString().toFloat()
+
+            val alturaQ2 = altura * altura
+            val resultado = peso / alturaQ2
+            println("Seu imc e" + resultado)
+        }
+
 
         }
     }
-}
